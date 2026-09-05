1e237136b906a6a7628ad697faafaa77a2ceacd7
brouezechiel <brouezechiel4@gmail.com>
2026-09-06 00:28:35 +0200

    Correction de QuestionService avec les méthodes ajouter et modifier

--- src/main/java/org/smf/enquetes/services/QuestionService.java
+++ src/main/java/org/smf/enquetes/services/QuestionService.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.smf.enquetes.dtos.request.QuestionRequestDTO;
 import org.smf.enquetes.dtos.response.QuestionResponseDTO;
 import org.smf.enquetes.entities.Question;
+import org.smf.enquetes.entities.Section;
 import org.smf.enquetes.exceptions.QuestionNonSupprimableException;
 import org.smf.enquetes.exceptions.QuestionNotFoundException;
+import org.smf.enquetes.exceptions.SectionNotFoundException;
 import org.smf.enquetes.mappers.QuestionMapper;
 import org.smf.enquetes.repositories.QuestionRepository;
 import org.smf.enquetes.repositories.ReponseRepository;
+import org.smf.enquetes.repositories.SectionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,8 +22,26 @@ import java.util.UUID;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+    private final SectionRepository sectionRepository;
     private final ReponseRepository reponseRepository;
     private final QuestionMapper questionMapper;
+
+    @Transactional
+    public QuestionResponseDTO ajouterQuestion(UUID sectionId, QuestionRequestDTO request) {
+        Section section = sectionRepository.findById(sectionId)
+                .orElseThrow(() -> new SectionNotFoundException(sectionId));
+
+        Question question = new Question();
+        question.setTexte(request.texte());
+        question.setType(request.type());
+        question.setOrdre(request.ordre());
+        question.setOptions(request.options());
+        question.setSection(section);
+        question.setEnquete(section.getEnquete());
+
+        Question savedQuestion = questionRepository.save(question);
+        return questionMapper.toResponseDTO(savedQuestion);
+    }
 
     @Transactional
     public QuestionResponseDTO modifierQuestion(UUID questionId, QuestionRequestDTO request) {
@@ -30,7 +51,7 @@ public class QuestionService {
         question.setTexte(request.texte());
         question.setType(request.type());
         question.setOrdre(request.ordre());
-        question.setOptions(request.options()); // Prise en compte des options à la modification
+        question.setOptions(request.options());
 
         Question updatedQuestion = questionRepository.save(question);
         return questionMapper.toResponseDTO(updatedQuestion);
